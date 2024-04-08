4ac33c12019883911b7c4ffd8d5b2e2ce64faf74
Jávor Márton <marton200472@gmail.com>
2024-04-08 21:07:13 +0200

    removed start and end dates

--- BoroHFR/Controllers/Api/ApiTasksController.cs
+++ BoroHFR/Controllers/Api/ApiTasksController.cs
@@ -21,13 +21,12 @@ public class ApiTasksController : ControllerBase
     public record ApiTask(DateTime deadline, string title, string description, bool done, string subject, string group, Guid userId);
 
     [HttpGet("")]
-    public async Task<IActionResult> GetTasks([FromQuery]DateOnly startDate, [FromQuery]DateOnly endDate, bool showDone = false, bool showNotDone = true) {
+    public async Task<IActionResult> GetTasks(bool showDone = false, bool showNotDone = true) {
         var user = await _dbContext.Users.Include(x=>x.AssociatedEvents).FirstAsync(x=>x.Id==this.GetUserId());
         var tasks = await _dbContext.Events
                         .Include(x=>x.Group).ThenInclude(x=>x.Subject)
                         .Where(x=>x.Group.Members.Contains(user) || (x.Group.Class.DefaultGroupId == x.Group.Id && user.ClassId == x.Group.ClassId))
                         .Where(x=>x.Type == Models.EventType.Task)
-                        .Where(x=>x.Date >= startDate && x.Date <= endDate)
                         .Where(x=>(x.AssociatedUsers.Contains(user) && showDone) || (!x.AssociatedUsers.Contains(user) && showNotDone))
                         .ToArrayAsync();
         var converted = tasks.Select(x=>new ApiTask(x.Date.ToDateTime(x.StartTime),x.Title, x.Description,
